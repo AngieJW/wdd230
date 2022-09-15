b61afc3ce55f15a44be2ba55dd745dfce8748fd0
angieJW <angie.duhard@gmail.com>
2022-09-16 00:47:52 +0200

    ADD lastmodified

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
 const copyYear = document.querySelector(copyYear);
 const currentYear = new Date().getFullYear();
 copyYear = currentYear;
+
+const lastModified = new Date(document.lastModified);
+document.getElementById("lastmodified").textContent = lastModified;
+
