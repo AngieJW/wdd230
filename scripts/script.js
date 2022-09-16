fbf209d6f032e3723e88b9f8019a8fd0e8750587
angieJW <angie.duhard@gmail.com>
2022-09-16 21:02:22 +0200

    edit

--- scripts/script.js
+++ scripts/script.js
@@ -2,5 +2,5 @@ const year = new Date().getFullYear();
 document.querySelector('#currentyear').textContent = year;
 
 const lastModified = new Date(document.lastModified);
-document.getElementById("lastmodified").textContent = lastModified;
+document.getElementById("lastmodified").innerHTML = lastModified;
 
