5411ce0a05f9e7a107cdcfc9de12802bc933c17a
angieJW <angie.duhard@gmail.com>
2022-10-29 23:57:54 +0200

    change discover js

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,27 +1,35 @@
 // LAZY LOADING //
-document.addEventListener("DOMContentLoaded", function() {
-    var lazyImages = [].slice.call(document.querySelectorAll(".lazy"));
-  
-    if ("IntersectionObserver" in window) {
-      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
-        entries.forEach(function(entry) {
-          if (entry.isIntersecting) {
-            let lazyImage = entry.target;
-            lazyImage.src = lazyImage.dataset.src;
-            lazyImage.srcset = lazyImage.dataset.srcset;
-            lazyImage.classList.remove("lazy");
-            lazyImageObserver.unobserve(lazyImage);
-          }
-        });
-      });
-  
-      lazyImages.forEach(function(lazyImage) {
-        lazyImageObserver.observe(lazyImage);
-      });
-    } else {
-      // Possibly fall back to event handlers here
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
     }
-  });
+
+    img.src = src;
+}
+const imgOptions =  {
+    threshold: 1,
+    rootsMargin: "0px 0px 50px 0px"
+};
+
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+        return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target)
+        }
+
+    })
+}, imgOptions);
+
+images.forEach(image => {
+    imgObserver.observe(image)
+})
 
 // NUMBER OF VISITS //
 let numberVisits = Number(window.localStorage.getItem("visits-ls"));
