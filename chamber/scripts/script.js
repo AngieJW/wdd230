d085f77e6ad08a31abb60f93124526ecf9e276b7
Angie JW <angie.duhard@gmail.com>
2023-05-13 21:54:54 +0200

    added chamber homepage

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,4 +1,18 @@
-const options = {year:"numeric"}
-document.querySelector("#currentyear").textContent = new Date().toLocaleDateString("en-US", options)
+const options = {
+    year: "numeric",
+}
+document.querySelector("#currentyear").textContent = new Date().toLocaleDateString("en-US", options);
 
-document.getElementById('lastmodified').innerHTML = (document.lastModified)
+let lastUpdate = (document.lastModified);
+document.getElementById("lastupdated").innerHTML = lastUpdate;
+
+const date = new Date();
+let fullDate = new Intl.DateTimeFormat("en-US", {dateStyle: "full"}).format(date);
+document.getElementById("currentdate").innerHTML = fullDate;
+
+function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerButton")
+x.onclick = toggleMenu;
